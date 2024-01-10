5f794c294464bbffcf12035c0a40689dbb8d4291
Fathima Fida T V <fidalatheef9@gmail.com>
2024-01-10 20:36:34 +0530

    Initial commit

---
+++ EmployeeCrud1/src/main/java/com/example/EmployeeCrud1/EmployeeCrud1Application.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeCrud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrud1Application.class, args);
+	}
+
+}
